be08bf25a5db423c3aeae300efff4bde9e798e0b
Me-Mr-Dean <birkholtzfrancoisdean@gmail.com>
2024-08-02 23:24:19 +0200

    Tested reflection emulator and changed naming convention of sigmoid function.

--- Crates/reflection_crate/src/main.rs
+++ Crates/reflection_crate/src/main.rs
@@ -1,3 +1,21 @@
+use std::any::Any;
+
+fn print_type_of<T: Any>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn get_type_of<T: Any>(_: &T) -> Box<dyn Any> {
+    let type_name = std::any::type_name::<T>();
+    return Box::new(type_name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = "hello";
+    print_type_of(&x); // Outputs: i32
+    print_type_of(&y); // Outputs: &str
+
+    let b = get_type_of(&y);
+    let c: b = "banana";
+    println!("{}", c);
 }
